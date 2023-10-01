d79ee0804b08ee01a34d0b221500adcce7b090c4
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-10-01 13:16:02 +0200

    move encoder into encodec

--- encodec.h
+++ encodec.h
@@ -127,3 +127,25 @@ struct encodec_model {
 
     std::map<std::string, struct ggml_tensor *> tensors;
 };
+
+struct ggml_tensor * strided_conv_1d(
+      ggml_context * ctx0,
+       ggml_tensor * inp,
+       ggml_tensor * conv_w,
+       ggml_tensor * conv_b,
+               int   stride);
+
+struct ggml_tensor * forward_pass_lstm_unilayer(
+      ggml_context * ctx0,
+       ggml_tensor * inp,
+       ggml_tensor * weight_ih,
+       ggml_tensor * weight_hh,
+       ggml_tensor * bias_ih,
+       ggml_tensor * bias_hh);
+
+struct ggml_tensor * strided_conv_transpose_1d(
+      ggml_context * ctx0,
+       ggml_tensor * inp,
+       ggml_tensor * conv_w,
+       ggml_tensor * conv_b,
+               int   stride);
