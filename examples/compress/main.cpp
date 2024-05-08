db7695647f786f1802e067cccf5c413d97f57ce9
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-05-08 20:18:18 +0200

    Updated examples accordingly

--- examples/compress/main.cpp
+++ examples/compress/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
     }
 
     // initialize encodec context
-    struct encodec_context * ectx = encodec_load_model(params.model_path, params.n_gpu_layers);
+    struct encodec_context * ectx = encodec_load_model(params.model_path.c_str(), 0 /* offset */, params.n_gpu_layers);
     if (!ectx) {
         printf("%s: error during loading model\n", __func__);
         return 1;
@@ -35,13 +35,19 @@ int main(int argc, char **argv) {
     }
 
     // compress audio
-    if (!encodec_compress_audio(ectx, original_audio_arr, params.n_threads)) {
+    float * audio_data = original_audio_arr.data();
+    int n_samples = original_audio_arr.size();
+    if (!encodec_compress_audio(ectx, audio_data, n_samples, params.n_threads)) {
         printf("%s: error during compression \n", __func__);
         return 1;
     }
 
     // write reconstructed audio on disk
-    if (!write_codes_to_file(params.output_path, ectx->out_codes, original_audio_arr.size())) {
+    int32_t * codes_data = encodec_get_codes(ectx);
+    int n_codes = encodec_get_codes_size(ectx);
+    std::vector<int32_t> codes_arr(codes_data, codes_data + n_codes);
+
+    if (!write_codes_to_file(params.output_path, codes_arr, original_audio_arr.size())) {
         printf("%s: error during writing codes to file\n", __func__);
         return 1;
     }
@@ -50,9 +56,11 @@ int main(int argc, char **argv) {
     {
         const int64_t t_main_end_us = ggml_time_us();
 
+        const encodec_statistics * stats = encodec_get_statistics(ectx);
+
         printf("\n\n");
-        printf("%s:     load time = %8.2f ms\n", __func__, ectx->t_load_us/1000.0f);
-        printf("%s:     eval time = %8.2f ms\n", __func__, ectx->t_compute_ms/1000.0f);
+        printf("%s:     load time = %8.2f ms\n", __func__, stats->t_load_us/1000.0f);
+        printf("%s:     eval time = %8.2f ms\n", __func__, stats->t_compute_us/1000.0f);
         printf("%s:    total time = %8.2f ms\n", __func__, (t_main_end_us - t_main_start_us)/1000.0f);
     }
 
