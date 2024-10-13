9ce851f8a322c84c45511457fcbe7e3a8b95f7c6
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-10-13 23:29:43 +0200

    move more encodec util func in util modules

--- utils.h
+++ utils.h
@@ -8,6 +8,30 @@
 const size_t MB = 1024 * 1024;
 
 template <typename T>
-static void read_safe(std::ifstream &infile, T &dest) {
+void read_safe(std::ifstream &infile, T &dest) {
     infile.read((char *)&dest, sizeof(T));
+}
+
+int32_t get_num_codebooks(float bandwidth, int hop_length, float sample_rate) {
+    // The number of codebooks is determined by the bandwidth selected.
+    // Supported bandwidths are 1.5kbps (n_q = 2), 3 kbps (n_q = 4), 6 kbps (n_q = 8),
+    // 12 kbps (n_q = 16) and 24kbps (n_q = 32).
+    return (int32_t)ceilf(1000 * bandwidth / (ceilf(sample_rate / hop_length) * 10));
+}
+
+int32_t get_bandwidth_per_quantizer(int bins, float frame_rate) {
+    return log2f((float)bins) * frame_rate;
+}
+
+int32_t get_num_quantizers_for_bandwidth(int bins, float frame_rate, float bandwidth) {
+    float bw_per_q = get_bandwidth_per_quantizer(bins, frame_rate);
+    int32_t n_q = MAX(1, floorf(bandwidth * 1000 / bw_per_q));
+    return n_q;
+}
+
+void ggml_log_callback_default(ggml_log_level level, const char *text, void *user_data) {
+    (void)level;
+    (void)user_data;
+    fputs(text, stderr);
+    fflush(stderr);
 }
